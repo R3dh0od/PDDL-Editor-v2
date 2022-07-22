32cbfdab01892e20c9c66e9c132ed84f3da144c9
R3dh0od <100476140@alumnos.uc3m.es>
2022-07-22 02:07:39 +0200

    terminado xml actions y states

--- src/components/XML/exportXMLStates.js
+++ src/components/XML/exportXMLStates.js
@@ -11,6 +11,13 @@ export default function ExportXMLStates(aux){
                     var itemChild=item.ele('predicate');
                     itemChild.att('description', params[i].predicate);
                     itemChild.att('not', params[i].not);
+                    if(params[i].params!==undefined){
+                        for(let j=0; j<params[i].params.length; j++){
+                            var predChild=itemChild.ele('object');
+                            predChild.att('variable', params[i].variables[j].variable);
+                            predChild.att('type', params[i].params[j].type);
+                        }
+                    }
                 }
 
             }
@@ -21,6 +28,22 @@ export default function ExportXMLStates(aux){
                     itemChild.att('operator', params[i].operator);
                     itemChild.att('firstValue', params[i].function);
                     itemChild.att('secondValue', params[i].function2);
+                    if(params[i].fParams!==undefined){
+                        var fxChild=itemChild.ele('firstVal');
+                        for(let j=0; j<params[i].fParams.length; j++){
+                            var fxChildElement= fxChild.ele('object')
+                            fxChildElement.att('variable', params[i].fVariables[j].variable);
+                            fxChildElement.att('type', params[i].fParams[j].type);
+                        }
+                    }
+                    if(params[i].f2Params!==undefined){
+                        var fxChild=itemChild.ele('secondVal');
+                        for(let j=0; j<params[i].f2Params.length; j++){
+                            var fxChildElement= fxChild.ele('object')
+                            fxChildElement.att('variable', params[i].f2Variables[j].variable);
+                            fxChildElement.att('type', params[i].f2Params[j].type);
+                        }
+                    }
                 }
 
             }
